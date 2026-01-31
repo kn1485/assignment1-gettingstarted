592a068b085da9daf42b9ace026229a915ef9d3b
kn1485 <kn1485@nyu.edu>
2026-01-31 03:50:21 -0500

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -16,27 +16,27 @@ def welcome_assignment_answers(question):
     #     answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
     # return(answer)
 # Complete all the questions.
-if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
         answer = "pcap"
-elif question == "Are encoding and encryption the same? - Yes/No":
+    elif question == "Are encoding and encryption the same? - Yes/No":
         answer= "no"
-elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer= "no"
-elif question == "Is it possible to decode a message without a key? - Yes/No":
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = "yes"
-elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "no"
-elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
-    answer= "54bbb8623c73c0fff3e7aa60a4291875"
-elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-    answer = "no"
-elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-    answer = "4"
-elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-    answer = "2"
-else:
-    answer = "no"
-return(answer)
+    elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+        answer= "54bbb8623c73c0fff3e7aa60a4291875"
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "no"
+    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+        answer = "4"
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+        answer = "2"
+    else:
+        answer = "no"
+    return(answer)
 
 if __name__ == "__main__":
     #use this space to debug and verify that the program works
@@ -53,4 +53,5 @@ if __name__ == "__main__":
 #"Is MD5 a secured hashing algorithm? - Yes/No":
 #"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
 #"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+
 

449818a0e06610c58f0255baf60c9443918394fb
kn1485 <kn1485@nyu.edu>
2026-01-31 03:46:41 -0500

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -5,18 +5,38 @@
 def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
-    if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "The student should type the answer here"
-    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "The student should type the answer here"
-    else: 
-        ### you should understand why this else case should be included
-        ### what happens if there is a typo in one of the questions?
-        ### maybe put something here to flag an issue and catch errors
-        answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
-    return(answer)
+    # if question == "Are encoding and encryption the same? - Yes/No":
+    #     answer = "The student should type the answer here"
+    # elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+    #     answer = "The student should type the answer here"
+    # else: 
+    #     ### you should understand why this else case should be included
+    #     ### what happens if there is a typo in one of the questions?
+    #     ### maybe put something here to flag an issue and catch errors
+    #     answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
+    # return(answer)
 # Complete all the questions.
-
+if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+        answer = "pcap"
+elif question == "Are encoding and encryption the same? - Yes/No":
+        answer= "no"
+elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+        answer= "no"
+elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "yes"
+elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "no"
+elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+    answer= "54bbb8623c73c0fff3e7aa60a4291875"
+elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+    answer = "no"
+elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+    answer = "4"
+elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+    answer = "2"
+else:
+    answer = "no"
+return(answer)
 
 if __name__ == "__main__":
     #use this space to debug and verify that the program works
@@ -33,3 +53,4 @@ if __name__ == "__main__":
 #"Is MD5 a secured hashing algorithm? - Yes/No":
 #"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
 #"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+
